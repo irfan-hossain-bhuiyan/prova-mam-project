4752cb90036ed8cad6235290ea6b9c5d981b0891
irfan-hossain-bhuiyan <irfanhbhy@gmail.com>
2023-10-05 09:50:55 +0600

    Added cursor to the input box Doing great.

--- graph/input_box.py
+++ graph/input_box.py
@@ -13,7 +13,7 @@ CYAN = pygame.Color(0, 255, 255)
 class InputBox:
     def __init__(
         self,
-        screen:pygame.Surface,
+        screen: pygame.Surface,
         x: int,
         y: int,
         width: int,
@@ -23,51 +23,79 @@ class InputBox:
         font_color: pygame.Color = BLACK,
         onEnter: Optional[Callable[['InputBox'], None]] = None,
         active: bool = True,
+        cursor_blink_interval: int = 500,  # Blink interval in milliseconds
     ):
-        self.screen=screen
+        self.screen = screen
         self.rect = pygame.Rect(x, y, width, height)
         self.color = CYAN
         self.text = ''
-        self.font=pygame.font.Font(None,32) if font is None else font
+        self.font = pygame.font.Font(None, 32) if font is None else font
         self.active = active
         self.onEnter = onEnter
         self.font_color = font_color
         self.rect_color = rect_color
+        self.cursor_visible = False
+        self.cursor_blink_interval = cursor_blink_interval
+        self.last_cursor_toggle = pygame.time.get_ticks()
+        self.cursor_position = 0  # Initial cursor position
 
     def handle_event(self, event):
         # Checking if the input box was clicked or not.
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos):
                 self.active = not self.active
+                self.cursor_visible = True
             else:
                 self.active = False
-            self.color = CYAN if self.active else WHITE
+                self.cursor_visible = False
         if event.type == pygame.KEYDOWN:
             if self.active:
                 if event.key == pygame.K_RETURN:
                     if self.onEnter is not None:
                         self.onEnter(self)
                 elif event.key == pygame.K_BACKSPACE:
-                    self.text = self.text[:-1]
+                    if self.cursor_position > 0:
+                        self.text = self.text[:self.cursor_position - 1] + self.text[self.cursor_position:]
+                        self.cursor_position -= 1
+                elif event.key == pygame.K_LEFT:
+                    if self.cursor_position > 0:
+                        self.cursor_position -= 1
+                elif event.key == pygame.K_RIGHT:
+                    if self.cursor_position < len(self.text):
+                        self.cursor_position += 1
                 else:
-                    self.text += event.unicode
+                    self.text = self.text[:self.cursor_position] + event.unicode + self.text[self.cursor_position:]
+                    self.cursor_position += 1
+
     def draw(self):
         pygame.draw.rect(self.screen, self.rect_color, self.rect, 2)
         txt_surface = self.font.render(self.text, True, self.font_color)
-        self.screen.blit(txt_surface, (self.rect.x + 5,self.rect.y + 5))
+        cursor_x = self.rect.x + 5 + self.font.size(self.text[:self.cursor_position])[0]
+
+        if self.cursor_visible and self.active:
+            cursor_y = self.rect.y + 5
+            pygame.draw.line(self.screen, self.font_color, (cursor_x, cursor_y), (cursor_x, cursor_y + self.font.get_height()), 2)
+
+        self.screen.blit(txt_surface, (self.rect.x + 5, self.rect.y + 5))
+
+    def update_cursor(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_cursor_toggle > self.cursor_blink_interval:
+            self.cursor_visible = not self.cursor_visible
+            self.last_cursor_toggle = current_time
 
 def main():
-# Initialize Pygame window
+    # Initialize Pygame window
     screen_width, screen_height = 1600, 900
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Input Box Example")
-    
+
     # Initialize font
     font = pygame.font.Font(None, 32)
-    
+
     # Create an InputBox instance
-    input_box = InputBox(screen,100, 50, 200, 40, font,onEnter=lambda x:print(x.text))
-    
+    input_box = InputBox(screen, 100, 50, 200, 40, font, onEnter=lambda x: print(x.text))
+
     # Main loop
     running = True
     while running:
@@ -75,15 +103,17 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             input_box.handle_event(event)
-    
+
+        input_box.update_cursor()
+
         screen.fill(WHITE)
         input_box.draw()
         # Draw the input box
-    
+
         pygame.display.flip()
-    
+
     pygame.quit()
     sys.exit()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()

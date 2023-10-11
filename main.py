8d2b3d52f7e7d04ae22d76c722aa9c55906789c4
irfan-hossain-bhuiyan <irfanhbhy@gmail.com>
2023-10-11 12:25:53 +0600

    there are 2 things in here.1:Adding x and y shift inbox2.Now I want to add event manageemnt in pygame,so I don't need to manually add other thing

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import pygame
 import sys
 from external_dependencies import color
@@ -7,36 +6,82 @@ from ui.input_box import InputBox
 import equation.equation_to_lines as eqTolines
 import equation.string_to_equatuion as strToeq
 GRAPH_RESOLUTION=20
+equation=None
+
+SCREEN_WIDTH ,SCREEN_HEIGHT =1080,760
+def from_left(x:int):
+    return x
+def from_right(x:int):
+    return SCREEN_WIDTH-x
+def from_top(y:int):
+    return y
+def from_down(y:int):
+    return SCREEN_HEIGHT-y
+
+
 def main():
    pygame.init() 
-   SCREEN_WIDTH ,SCREEN_HEIGHT =1080,760
-   screen=pygame.display.set_mode((1080,760))
+   screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
    lines=[]
    graph=Graph(x=0,y=0,screen=screen,width=SCREEN_WIDTH,height=SCREEN_HEIGHT)
-   def onUserInput(inputBox:InputBox):
+   def onInputBoxEnter(inputBox:InputBox):
        input=inputBox.text
+       if input=="":
+           return
        try:
            equation=strToeq.convert_to_standard_form(input)
            contour=eqTolines.equation_to_line_func(equation)
+           inputBox.text=str(equation)+"=0"
            nonlocal lines
-           lines=contour(-graph.max_x(),graph.max_x(),-graph.max_y(),graph.max_y(),graph.max_x()*GRAPH_RESOLUTION,graph.max_y()*GRAPH_RESOLUTION)
+           lines=contour(-graph.max_x(),graph.max_x(),-graph.max_y(),graph.max_y()\
+                   ,graph.max_x()*GRAPH_RESOLUTION,graph.max_y()*GRAPH_RESOLUTION)
        except:
            raise ValueError("Invalid equation is given")
-   inbox=InputBox(screen,x=10,y=700,width=1060,height=50,\
-           onEnter=onUserInput,placeholder="Enter equation:")
+   def onXInputEnter(box:InputBox): 
+       if equation is None:
+           return
+       text=box.text
+       if text=="":
+           return
+       box.text=""
+       text=float(text)
+       #equation=equation as 
+       print("text")
+   def onYInputEnter(box:InputBox):
+        if equation is None:
+            return 
+        text=box.text
+        if text=="":
+            return
+        box.text=""
+        print("text")
+
+        
+
+       ##euation box positioning.
+   inbox=InputBox(screen,x=from_left(10),y=from_down(60),width=SCREEN_WIDTH-20,height=50,\
+           onEnter=onInputBoxEnter,placeholder="Enter equation:")
+   x_input=InputBox(screen,x=from_right(200),y=from_down(110),width=150,\
+           height=40,onEnter=onXInputEnter,placeholder="Enter x shift:")
+   y_input=InputBox(screen,x=from_right(200),y=from_down(160),width=150,\
+           height=40,onEnter=onYInputEnter,placeholder="Enter y shift:")
+   components=[inbox,x_input,y_input,graph]
    running=True
    while running:
         for event in pygame.event.get():
-            inbox.handle_event(event)
             if event.type == pygame.QUIT:
                 running = False
+            for component in components:
+                try:
+                    component.handle_event(event)
+                except:
+                    pass
         screen.fill(color.WHITE)
+        for component in components:
+            component.draw()
         # Draw the graph
-        graph.draw()
-
         for x in lines:
             graph.draw_linesC(x,width=3)
-        inbox.draw()
         pygame.display.flip()
    pygame.quit()
    sys.exit()
